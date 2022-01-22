73e20792c3c26e17030670cd083bd544f390cc70
Artem Eremeev <jeremy1002@yandex.ru>
2022-01-22 12:43:39 +0300

    feat: add public RequestParameters initializer

--- Sources/WebClient/Public/RequestParameters.swift
+++ Sources/WebClient/Public/RequestParameters.swift
@@ -9,4 +9,20 @@ public struct RequestParameters {
     let additionalHeaders: [RequestHeader]
     let requestProgress: RequestProgress?
     
+    public init(baseURL: URL,
+                method: HTTPMethod = .get,
+                body: RequestBody = .none,
+                queryItems: [URLQueryItem] = [],
+                additionalHeaders: [RequestHeader] = [],
+                requestProgress: RequestProgress? = nil) {
+        
+        self.baseURL = baseURL
+        self.method = method
+        self.body = body
+        self.queryItems = queryItems
+        self.additionalHeaders = additionalHeaders
+        self.requestProgress = requestProgress
+        
+    }
+    
 }
